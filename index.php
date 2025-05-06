724faecff4567628cc544a1f0f9193eb3f2ca3f4
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-05-06 19:15:33 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("header.php"); ?>
+
+<h4 style="color: black;">Seja Bem-Vindo!</h4>
+
+<div class="d-flex gap-3" style="margin-top: 20px;">
+    <div class="card bg-success" style="width: 350px; color: white">
+    <div class="card-body">
+        <h4 class="card-title">Vendas</h4>
+        <p class="card-text">Some example text. Some example text.</p>
+        <a href="#" class="card-link"></a>
+        <a href="#" class="card-link"></a>
+    </div>
+    </div>
+
+    <div class="card bg-danger" style="width: 350px; color: white">
+    <div class="card-body">
+        <h4 class="card-title">Despesas</h4>
+        <p class="card-text">Some example text. Some example text.</p>
+        <a href="#" class="card-link"></a>
+        <a href="#" class="card-link"></a>
+    </div>
+    </div>
+
+    <div class="card bg-primary" style="width: 350px; color: white">
+    <div class="card-body">
+        <h4 class="card-title">Faturamento</h4>
+        <p class="card-text">Some example text. Some example text.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link"></a>
+    </div>
+    </div>
+
+</div>
+
+<?php include("footer.php"); ?>
