8c352b14ec7b729fab1192351c3e5db68636ed53
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-05-19 10:21:26 -0300

    Update produtos.php

--- view/produtos.php
+++ view/produtos.php
@@ -8,7 +8,10 @@
     <div class="collapse navbar-collapse" id="mynavbar">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="formProdutos.php">Cadastrar novo</a>
+          <a class="nav-link" href="formProdutos.php">Cadastrar novo Produto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="formProdutos.php">Cadastrar nova Categoria</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
