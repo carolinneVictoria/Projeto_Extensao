2718a6b4b297df56bba76a5b480baab53b036882
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-05-25 20:15:19 -0300

    Create verProduto.php

---
+++ view/ProdutoView/verProduto.php
@@ -0,0 +1,85 @@
+<?php include("../../app/header.php");
+
+include ('../../config/conexaoBD.php');
+include ('../../model/Produto.php');
+include ('../../model/Categoria.php');
+
+$produtoModel = new Produto($conn);
+$categoriaModel = new Categoria($conn);
+
+if (isset($_GET['id'])) {
+    $idProduto = $_GET['id'];
+    
+    $produto = $produtoModel->buscarProdutoPorId($idProduto);
+    $categoria = $categoriaModel->listarCategoria();
+} else {
+    echo "ID do produto não informado!";
+    exit(); 
+}
+?>
+
+
+<div class="container-fluid text-left">
+
+    <h3>Detalhes do Produto:</h3>
+    
+    <div class="col-sm-12">
+        
+        <input type="hidden" name="idProduto" value="<?= $produto['idProduto']; ?> ">
+
+        <div class="row">
+        <div class="col-md-6 mb-3 border" >
+            <div class="form-floating">
+                <input type="text" class="form-control border border-info rounded" id="nomeProduto" name="nomeProduto" value="<?= $produto['nomeProduto']; ?>" required readonly>
+                <label for="nomeProduto">Nome do Produto:</label>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control border border-info rounded" id="valorProduto" name="valorProduto" value="<?= $produto['valorProduto']; ?>" required readonly>
+                <label for="valorProduto">Valor do Produto:</label>
+            </div>
+        </div>
+
+        <div class="col-md-12 mb-3">
+            <div class="form-floating">
+                <textarea style="height: 100px" class="form-control border border-info rounded" id="descricaoProduto" name="descricaoProduto" readonly><?= $produto['descricaoProduto']; ?></textarea>
+                <label for="descricaoProduto">Descrição do Produto:</label>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-3">
+            <div class="form-floating">
+                <select class="form-control border border-info rounded" id="categoriaProduto" name="categoriaProduto" required readonly>
+                    <?php foreach ($categoria as $categorias): ?>
+                        <option value="<?= $categorias['idCategoria']; ?>" 
+                            <?= $categorias['idCategoria'] == $produto['idCategoria'] ? 'selected' : ''; ?>>
+                            <?= $categorias['descricao']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            <label for="categoriaProduto">Categoria do Produto:</label>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-3">
+            <div class="form-floating">
+                <input type="text" class="form-control border border-info rounded" id="quantidadeProduto" name="quantidadeProduto" value="<?= $produto['quantidadeProduto']; ?>" required readonly>
+                <label for="quantidadeProduto">Quantidade:</label>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end mt-3">
+            <a href="produtos.php" class="btn btn-secondary btn me-2">Voltar</a>
+        </div>
+
+        </div>
+
+    </form>
+    </div>
+    
+
+</div>
+
+<?php include("../../app/footer.php") ?>
