bcfbd44099e92fca06b9d1c3fa1086ef110ac46a
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-06-13 20:44:59 -0300

    Update clientes.php

--- view/ClienteView/clientes.php
+++ view/ClienteView/clientes.php
@@ -1,37 +1,39 @@
 <?php include ("../../app/header.php"); ?>
-
 <!-- Navbar e Barra de Busca -->
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container w-100">
-    <div class="collapse navbar-collapse" id="mynavbar">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/Projeto_Extensao/view/ClienteView/formCliente.php?acao=cadastrar">Cadastrar novo Cliente</a>
-        </li>
-      </ul>
-      <form method="GET" action="/Projeto_Extensao/controller/ClienteController.php" class="d-flex" role="search">
-        <input type="hidden" name="acao" value="buscar">
-        <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome" value="<?= $_GET['busca'] ?? '' ?>">
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
-      </form>
+<div class="container-fluid bg-dark d-flex position-fixed" style="top: 50px; left: 160px; width: calc(100% - 160px); height: 50px; z-index: 1030;">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark w-100">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="mynavbar">
+          <ul class="navbar-nav mb-4 mb-lg-0">
+            <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/ClienteView/formCliente.php?acao=cadastrar">Cadastrar novo Cliente</a></li>
+          </ul>
+        <!-- Campo de busca -->
+        <form method="GET" action="/Projeto_Extensao/controller/ClienteController.php?acao=buscar" class="d-flex me-2" role="search">
+          <input type="hidden" name="acao" value="buscar">
+          <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome"value="<?= $_GET['busca'] ?? '' ?>">
+          <button class="btn btn-outline-light" type="submit">Buscar</button>
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
+
+<!-- Conteúdo principal -->
+<div class="container" style="margin-left: -10px; padding-top: 50px;">
 
 <?php
-
 include ('../../config/conexaoBD.php');
 require_once ('../../model/Cliente.php');
+
 // Instanciando o Model
 $clienteModel = new Cliente($conn);
+
 // Listando os clientes
 $clientes = $clienteModel->listarClientes();
-
 echo "<h5>Clientes cadastrados:</h5>";
 
 // Exibindo a tabela de clientes
 echo "
-
     <table class='table table-hover table-bordered table-sm'>
         <thead class='thead-light'>
             <tr>
@@ -66,15 +68,8 @@ while ($registro = mysqli_fetch_assoc($clientes)) {
     ";
 }
 echo "</table>";
-
-
-if (isset($_GET['id'])) {
-    $idCliente = $_GET['id'];
-    $cliente = $clienteModel->excluirCliente($idCliente);
-}
-
 ?>
 
 <?php include "../../app/footer.php"; ?>
 
-?>
+</div>
