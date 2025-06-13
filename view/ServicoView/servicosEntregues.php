714ceafcfdcc7f7c730b3677fe2559a4d0580e31
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-06-13 16:01:11 -0300

    Update servicosEntregues.php

--- view/ServicoView/servicosEntregues.php
+++ view/ServicoView/servicosEntregues.php
@@ -73,8 +73,7 @@ while ($registro = mysqli_fetch_assoc($servicos)) {
                 <td>" . ($registro['entrega'] == 0 ? 'Sim' : 'Não') . "</td>
                 <td>{$registro['valorTotal']}</td>
                 <td>
-                <a href='formAtualizarServico.php?id=$idServico' class='btn btn-primary btn-sm'>Atualizar</a>
-                <a href='../../controller/ServicoController.php?acao=excluir&id=$idServico' class='btn btn-danger btn-sm' onclick='return confirm(\"Tem certeza que deseja excluir?\")'>Excluir</a>
+                <a href='verServico.php?id=$idServico' class='btn btn-primary brn-sm'>Ver Detalhes</a>
                 </td>
             </tr>
         </tbody>
