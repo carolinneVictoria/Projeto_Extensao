bbf0a9d868c0c42eb5d11506f0e19cb7e6d560e7
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-06-13 20:45:02 -0300

    Update fornecedores.php

--- view/FornecedorView/fornecedores.php
+++ view/FornecedorView/fornecedores.php
@@ -1,26 +1,30 @@
 <?php include ("../../app/header.php"); ?>
-
 <!-- Navbar e Barra de Busca -->
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container w-100">
-    <div class="collapse navbar-collapse" id="mynavbar">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/Projeto_Extensao/view/FornecedorView/formFornecedor.php?acao=cadastrar">Cadastrar novo Fornecedor</a>
-        </li>
-      </ul>
-      <form method="GET" action="/Projeto_Extensao/controller/FornecedorController.php" class="d-flex" role="search">
-        <input type="hidden" name="acao" value="buscar">
-        <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome" value="<?= $_GET['busca'] ?? '' ?>">
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
-      </form>
+<div class="container-fluid bg-dark d-flex position-fixed" style="top: 50px; left: 160px; width: calc(100% - 160px); height: 50px; z-index: 1030;">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark w-100">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="mynavbar">
+          <ul class="navbar-nav mb-4 mb-lg-0">
+            <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/FornecedorView/formFornecedor.php?acao=cadastrar">Cadastrar novo Fornecedor</a></li>
+          </ul>
+        <!-- Campo de busca -->
+        <form method="GET" action="/Projeto_Extensao/controller/FornecedorController.php?acao=buscar" class="d-flex me-2" role="search">
+          <input type="hidden" name="acao" value="buscar">
+          <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome"value="<?= $_GET['busca'] ?? '' ?>">
+          <button class="btn btn-outline-light" type="submit">Buscar</button>
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
+
+<!-- Conteúdo principal -->
+<div class="container" style="margin-left: -10px; padding-top: 50px;">
 
 <?php
 include ('../../config/conexaoBD.php');
 require_once ('../../model/Fornecedor.php');
+
 // Instanciando o Model
 $fornecedorModel = new Fornecedor($conn);
 // Listando os clientes
@@ -60,13 +64,7 @@ while ($registro = mysqli_fetch_assoc($fornecedores)) {
     ";
 }
 echo "</table>";
-
-if (isset($_GET['id'])) {
-    $idFornecedor = $_GET['id'];
-    
-    $fornecedor = $fornecedorModel->excluirFornecedor($idFornecedor);
-} 
-
 ?>
-
 <?php include "../../app/footer.php"; ?>
+
+</div>
