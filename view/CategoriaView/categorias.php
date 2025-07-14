60182b4cda3194821d0ee1caca26f0e6808e4ebd
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-07-14 17:29:50 -0300

    Update categorias.php

--- view/CategoriaView/categorias.php
+++ view/CategoriaView/categorias.php
@@ -1,27 +1,31 @@
 <?php include (__DIR__."/../../app/header.php"); ?>
 
-
 <!-- Navbar e Barra de Busca -->
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container w-100">
-    <div class="collapse navbar-collapse" id="mynavbar">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/Projeto_Extensao/view/ProdutoView/produtos.php">Produtos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#modalCategoria">Cadastrar Categoria</a>
-        </li>
-      </ul>
-        <form method="GET" action="/Projeto_Extensao/controller/ProdutoController.php" class="d-flex" role="search">
-        <input type="hidden" name="acao" value="buscar">
-        <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome" value="<?= $_GET['busca'] ?? '' ?>">
-        <button class="btn btn-outline-light" type="submit">Buscar</button>
+<div class="container-fluid bg-dark d-flex position-fixed" style="top: 50px; left: 160px; width: calc(100% - 160px); height: 50px; z-index: 1030;">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark w-100">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="mynavbar">
+          <ul class="navbar-nav mb-4 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="/Projeto_Extensao/view/ProdutoView/produtos.php">Produtos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#modalCategoria">Cadastrar Categoria</a>
+            </li>
+          </ul>
+        <!-- Campo de busca -->
+        <form method="GET" action="/Projeto_Extensao/controller/ProdutoController.php?acao=buscar" class="d-flex me-2" role="search">
+          <input type="hidden" name="acao" value="buscar">
+          <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome"value="<?= $_GET['busca'] ?? '' ?>">
+          <button class="btn btn-outline-light" type="submit">Buscar</button>
         </form>
-
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</div>
+
+<!-- Conteúdo principal -->
+<div class="container" style="margin-left: -10px; padding-top: 50px;">
 
 <?php
 
@@ -85,7 +89,7 @@ while ($registro = mysqli_fetch_assoc($categorias)) {
                     </div>
                   </div>
 
-                <a href='/Projeto_Extensao/controller/CategoriaController.php?acao=excluir&id=$idCategoria' class='btn btn-danger btn-sm' onclick='return confirm(\"Tem certeza que deseja excluir?\")'>Excluir</a>
+                <a href=\"/Projeto_Extensao/controller/CategoriaController.php?acao=excluir&id={$idCategoria}\" class='btn btn-danger btn-sm' onclick=\"return confirm('A categoria possui associação com produtos. Dessa forma, se for excluida, excluira o produto também.')\">Excluir</a>
                 </td>
             </tr>
         </tbody>
@@ -121,3 +125,4 @@ echo "</table>";
 
 
 <?php include "../../app/footer.php"; ?>
+</div>
