975c93d0ba85eae05088396146545ed59664e092
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-07-28 16:10:34 -0300

    Update header.php

--- app/header.php
+++ app/header.php
@@ -66,13 +66,14 @@ $primeiroNome = $nomeUsuario ? explode(' ', $nomeUsuario)[0] : '';
             </div>
 
             <ul class="navbar-nav w-100">
-                <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/index.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/index.php">Dashboard</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/VendaView/vendas.php">Vendas</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/ProdutoView/produtos.php">Produtos</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/ServicoView/servicos.php">Serviços</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/ClienteView/clientes.php">Clientes</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/FornecedorView/fornecedores.php">Fornecedores</a></li>
                     <li class="nav-item"><a class="nav-link" href="../estoque.php">Estoque</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/VendaView/vendas.php">Vendas</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/FinanceiroView/contas.php">Financeiro</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/view/UsuarioView/usuarios.php">Usuarios</a></li>
                     <li class="nav-item"><a class="nav-link" href="/Projeto_Extensao/controller/logout.php">Logout</a></li>
             </ul>
