844846ea4edd01857406deb2872ac7b6e5491b69
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-07-28 17:08:18 -0300

    Update Financeiro.php

--- model/Financeiro.php
+++ model/Financeiro.php
@@ -27,6 +27,53 @@ private $conn;
         return $res;
     }
 
+    public function atualizarConta($idConta, $descricao, $valorTotal, $dataVencimento, $status){
+        $atualizar = "UPDATE Financeiro
+                        SET descricao       = '$descricao',
+                            valorTotal      = '$valorTotal',
+                            dataVencimento  = '$dataVencimento',
+                            status          = '$status'
+
+                        WHERE idConta = '$idConta'
+                    ";
+        $res = mysqli_query($this->conn, $atualizar);
+        return $res;
+    }
+
+    public function excluirConta($idConta) {
+        $excluir = "DELETE FROM Financeiro WHERE idConta=?";
+        $stmt = $this->conn->prepare($excluir);
+        if ($stmt === false) {
+            echo "Erro na preparação de consulta.";
+            return false;
+        }
+        $stmt->bind_param("i", $idConta);
+        return ($stmt->execute());
+    }
+
+    public function buscarPorNome($termo) {
+        $buscar = "SELECT * FROM Financeiro WHERE descricao LIKE ?";
+        $stmt = $this->conn->prepare($buscar);
+        $like = "%" . $termo . "%";
+        $stmt->bind_param("s", $like);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return $res;
+    }
+
+    public function buscarContaPorId($idConta) {
+    $stmt = $this->conn->prepare("SELECT * FROM Financeiro WHERE idConta = ?");
+    $stmt->bind_param("i", $idConta);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()) {
+            return $row;
+        } else {
+            return null;
+        }
+    }
+
 }
 
 ?>
