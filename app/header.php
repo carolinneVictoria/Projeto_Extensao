bab0ab69c5cabcf46742d077c3d007372a30bb5c
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-07-28 19:57:27 -0300

    Update header.php

--- app/header.php
+++ app/header.php
@@ -41,6 +41,8 @@ $primeiroNome = $nomeUsuario ? explode(' ', $nomeUsuario)[0] : '';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <script>
         $(document).ready(function(){
             $("#telefoneUsuario").mask("(00) 00000-0000");
