5a131a2b32d19492b310812360c418502597cbd4
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-07-28 19:57:31 -0300

    Update FinanceiroController.php

--- controller/FinanceiroController.php
+++ controller/FinanceiroController.php
@@ -8,6 +8,15 @@ function listarContas($financeiroModel) {
     $contas = $financeiroModel->listarContas();
     include('../view/FinanceiroView/contas.php');
 }
+
+function filtrarContas($financeiroModel) {
+    $mes = $_GET['mes'] ?? null;
+    $ano = $_GET['ano'] ?? null;
+
+    $contas = $financeiroModel->listarContasPorMesEAno($mes, $ano);
+    include('../view/FinanceiroView/contas.php');
+}
+
 function cadastrarConta($financeiroModel) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $descricao      = $_POST['descricao'];
@@ -23,6 +32,7 @@ function cadastrarConta($financeiroModel) {
         }
     }
 }
+
 function atualizarConta($financeiroModel) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['idConta'])) {
         $idConta           = $_POST['idConta'];
@@ -35,12 +45,12 @@ function atualizarConta($financeiroModel) {
             header("Location: ../view/FinanceiroView/contas.php");
             exit();
         } else {
-        global $conn;
-        echo "Erro ao atualizar a conta! " . mysqli_error($conn);
-
+            global $conn;
+            echo "Erro ao atualizar a conta! " . mysqli_error($conn);
         }
     }
 }
+
 function excluirConta($financeiroModel) {
     $idConta = $_GET['id'];
     $resultado = $financeiroModel->excluirConta($idConta);
@@ -50,35 +60,35 @@ function excluirConta($financeiroModel) {
         echo "ERRO AO EXCLUIR!";
     }
 }
+
 function buscarConta($financeiroModel) {
     if (isset($_GET['busca'])) {
-            $termo = $_GET['busca'];
-            $fornecedores = $financeiroModel->buscarPorNome($termo);
-            include('../view/FinanceiroView/verBuscaConta.php');
+        $termo = $_GET['busca'];
+        $fornecedores = $financeiroModel->buscarPorNome($termo);
+        include('../view/FinanceiroView/verBuscaConta.php');
     } else {
         echo "NENHUM TERMO DE BUSCA INFORMADO.";
     }
 }
 
-
+// Controle das rotas (ações)
 if (isset($_GET['acao'])) {
     $acao = $_GET['acao'];
 
-    // Chamando a ação de acordo com a URL
     if ($acao == 'cadastrar') {
         cadastrarConta($financeiroModel);
     } elseif ($acao == 'listar') {
         listarContas($financeiroModel);
     } elseif ($acao == 'atualizar') {
-        atualizarConta($financeiroModel); // Se o formulário for enviado, processa a atualização
+        atualizarConta($financeiroModel);
     } elseif ($acao == 'excluir') {
         excluirConta($financeiroModel);
-    } elseif($acao == 'buscar') {
+    } elseif ($acao == 'buscar') {
         buscarConta($financeiroModel);
+    } elseif ($acao == 'filtrar') {
+        filtrarContas($financeiroModel);
     }
 } else {
-    // Caso nenhuma ação seja especificada, exibe a listagem
     listarContas($financeiroModel);
 }
-
 ?>
