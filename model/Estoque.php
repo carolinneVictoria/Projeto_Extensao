37caa549a95c437e537fd27ad7333e473af39c91
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-07 11:41:19 -0300

    Update Estoque.php

--- model/Estoque.php
+++ model/Estoque.php
@@ -37,6 +37,58 @@ class Estoque {
         return $res;
     }
 
+    public function atualizarCompra($idCompra, $idFornecedor, $idUsuario, $data, $valorTotal, $descricao) {
+        $atualizar = "UPDATE Compra
+                        SET  idFornecedor    = '$idFornecedor',
+                             idUsuario       = '$idUsuario',
+                             data            = '$data',
+                             valorTotal      = '$valorTotal',
+                             descricao       = '$descricao'
+                             
+                            WHERE idCompra = '$idCompra'
+                        ";
+        $res = mysqli_query($this->conn, $atualizar);
+        return $res;
+    }
+
+    public function excluirCompra($idCompra) {
+        $excluir = "DELETE FROM Compra WHERE idCompra=?";
+        $stmt = $this->conn->prepare($excluir);
+        if ($stmt === false) {
+            echo "Erro na preparação de consulta.";
+            return false;
+        }
+        $stmt->bind_param("i", $idCompra);
+        return ($stmt->execute());
+    }
+
+    public function buscarPorNome($termo) {
+        $buscar = "SELECT * FROM Compra WHERE descricao LIKE ?";
+        $stmt = $this->conn->prepare($buscar);
+        $like = "%" . $termo . "%";
+        $stmt->bind_param("s", $like);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return $res;
+    }
+
+    public function buscarCompraPorId($idCompra) {
+    $stmt = $this->conn->prepare("SELECT Compra.*, Usuario.nomeUsuario, Fornecedor.razaoSocial
+                                    FROM Compra
+                                    INNER JOIN Fornecedor ON Compra.idFornecedor = Fornecedor.idFornecedor
+                                    INNER JOIN Usuario ON Compra.idUsuario       = Usuario.idUsuario
+                                    WHERE Compra.idCompra = ?");
+    $stmt->bind_param("i", $idCompra);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($row = $result->fetch_assoc()) {
+        return $row;
+    } else {
+        return null;
+    }
+}
+
 }
 
 ?>
