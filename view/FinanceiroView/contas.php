753bf403a1a486da31ad8aa41f9fde0cd35078b1
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-08 17:30:39 -0300

    Update contas.php

--- view/FinanceiroView/contas.php
+++ view/FinanceiroView/contas.php
@@ -71,7 +71,7 @@
 
   while ($registro = mysqli_fetch_assoc($contas)) {
     $idConta = $registro['idConta'];
-    $status = $registro['status'] == 0 ? 'A pagar' : 'A pagar';
+    $status = $registro['status'] == 0 ? 'A pagar' : 'Pago!';
     $valor = number_format($registro['valorTotal'], 2, ',', '.');
 
     echo "
