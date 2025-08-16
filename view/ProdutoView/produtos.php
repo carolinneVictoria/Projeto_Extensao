bd3335b61be3ab63867cd9206c9cd0ad992701e4
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-15 23:38:15 -0300

    Update produtos.php

--- view/ProdutoView/produtos.php
+++ view/ProdutoView/produtos.php
@@ -1,30 +1,26 @@
 <?php include ("../../app/header.php"); ?>
-<!-- Navbar e Barra de Busca -->
-<div class="container-fluid bg-dark d-flex position-fixed" style="top: 50px; left: 160px; width: calc(100% - 160px); height: 50px; z-index: 1030;">
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark w-100">
-    <div class="container-fluid">
-      <div class="collapse navbar-collapse d-flex justify-content-between" id="mynavbar">
-          <ul class="navbar-nav mb-4 mb-lg-0">
-          <li class="nav-item"><a class="nav-link" href="formProdutos.php">Cadastrar novo Produto</a></li>
-          <li class="nav-item"><a class="nav-link" href="../CategoriaView/categorias.php">Categorias</a></li>
-        </ul>
-        <!-- Campo de busca -->
-        <form method="GET" action="/Projeto_Extensao/controller/ProdutoController.php?acao=buscar" class="d-flex me-2" role="search">
-          <input type="hidden" name="acao" value="buscar">
-          <input type="text" name="busca" class="form-control me-2" placeholder="Buscar por Nome"value="<?= $_GET['busca'] ?? '' ?>">
-          <button class="btn btn-outline-light" type="submit">Buscar</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-</div>
 
 <!-- Conteúdo principal -->
-<div class="container" style="margin-left: -10px; padding-top: 50px;"></div>
+<div class="container" style="margin-left: -10px; padding-top: 10px;"></div>
+    <!-- Título e botão -->
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h2>Lista de Produtos</h2>
+        <a href="formProdutos.php" class="btn btn-success"><i class="fas fa-plus"></i> Novo Produto</a>
+    </div>
+
+    <!-- Formulário de busca -->
+    <form action="/Projeto_Extensao/controller/ProdutoController.php?acao=buscar" method="GET" class="mb-3">
+        <div class="input-group">
+            <input type="text" name="busca" class="form-control" placeholder="Pesquisar produto...">
+            <button class="btn btn-primary" type="submit" name="acao" value="buscar">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+    </form>
 
 <?php
 // Inclui a conexão ao banco e o modelo de Produto
-include_once "../../config/conexaoBD.php"; 
+include_once "../../config/conexaoBD.php";
 include_once "../../model/Produto.php";
 
 // Instanciando o Model
@@ -32,35 +28,42 @@ $produtoModel = new Produto($conn);
 
 // Listando os produtos
 $produtos = $produtoModel->listarProdutos();
-$totalProdutos = mysqli_num_rows($produtos); 
-
-echo "<p></p><h5>$totalProdutos produtos cadastrados.</h5><p></p>";
+$totalProdutos = mysqli_num_rows($produtos);
 
 // Exibindo a tabela de produtos
 echo "
-    <table class='table table-hover table-bordered table-sm'>
-        <thead class='thead-light'>
-            <tr>
-                <th>NOME</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>CATEGORIA</th>
-                <th>AÇÕES</th>
-            </tr>
-        </thead>";
+    <div class='table-responsive'>
+        <table class='table table-striped align-middle'>
+            <thead class='table-dark'>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Quantidade</th>
+                    <th>Preço (R$)</th>
+                    <th>Categoria</th>
+                    <th class='text-center'>Ações</th>
+                </tr>
+            </thead>";
 
 while ($registro = mysqli_fetch_assoc($produtos)) {
   $idProduto = $registro['idProduto'];
     echo "
         <tbody>
             <tr>
+                <td>{$registro['idProduto']}</td>
                 <td>{$registro['nomeProduto']}</td>
                 <td>{$registro['quantidadeProduto']}</td>
                 <td>R$ " . number_format($registro['valorProduto'], 2, ',', '.') . "</td>
                 <td>{$registro['descricao']}</td>
-                <td>
-                <a href='verProduto.php?id=$idProduto' class='btn btn-primary btn-sm'>Ver Detalhes</a>
+                <td class='text-center'>
+                  <a class='btn btn-warning btn-sm' href='verProduto.php?id=$idProduto'>
+                      <i class='fas fa-edit'></i>
+                  </a>
+                  <a class='btn btn-danger btn-sm' href='../../controller/ProdutoController.php?acao=excluir&id=$idProduto' onclick=\"return confirm('Tem certeza que deseja excluir?')\">
+                    <i class='fas fa-trash'></i>
+                  </a>
                 </td>
+
             </tr>
         </tbody>
     ";
