b3c894a884998f2588985d81ebfc5c90808ff7ab
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-18 15:24:04 -0300

    Update verCliente.php

--- view/ClienteView/verCliente.php
+++ view/ClienteView/verCliente.php
@@ -1,85 +1,77 @@
-<?php include("../../app/header.php");
+<?php include("../app/header.php"); ?>
 
-include ('../../config/conexaoBD.php');
-include ('../../model/Cliente.php');
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-dark text-white text-center">
+                    <h4 class="mb-0">Detalhes do Cliente</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <input type="hidden" name="idCliente" value="<?= htmlspecialchars($cliente['idCliente']); ?>">
 
-$clienteModel = new Cliente($conn);
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="nome" name="nome" value="<?= htmlspecialchars($cliente['nome']); ?>" readonly>
+                                    <label for="nome">Nome:</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="telefone" name="telefone" value="<?= htmlspecialchars($cliente['telefone']); ?>" readonly>
+                                    <label for="telefone">Telefone:</label>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cpf" name="cpf" value="<?= htmlspecialchars($cliente['cpf']); ?>" readonly>
+                                    <label for="cpf">CPF:</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="date" class="form-control" id="dataNascimento" name="dataNascimento" value="<?= htmlspecialchars($cliente['dataNascimento']); ?>" readonly>
+                                    <label for="dataNascimento">Data de Nascimento:</label>
+                                </div>
+                            </div>
 
-if (isset($_GET['id'])) {
-    $idCliente = $_GET['id'];
-    
-    $cliente = $clienteModel->buscarClientePorId($idCliente);
-} else {
-    echo "ID do Cliente não informado!";
-    exit(); 
-}
-?>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="endereco" name="endereco" value="<?= htmlspecialchars($cliente['endereco']); ?>" readonly>
+                                    <label for="endereco">Endereço</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="bicicleta" name="bicicleta" value="<?= htmlspecialchars($cliente['bicicleta']); ?>" readonly>
+                                    <label for="bicicleta">Bicicleta</label>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <hr class="my-4">
 
-<div class="container-fluid">
-    <h4>Detalhes do Cliente:</h4>
- 
-    <div class="col-sm-12">
-
-        <form action="../../controller/ClienteController.php?acao=atualizar" method="POST" enctype="multipart/form-data" class="was-validated">
-
-            <input type="hidden" name="idCliente" value="<?= $cliente['idCliente']; ?> ">
-
-            <div class="row">
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control border border-info rounded" id="nome" name="nome" value="<?= htmlspecialchars($cliente['nome']); ?>" required readonly>
-                    <label for="nome">Nome:</label>
+                        <div class="d-flex justify-content-between">
+                            <a href="javascript:history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </a>
+                            <div>
+                                <a href='../controller/ClienteController.php?acao=formAtualizar&id=<?= htmlspecialchars($cliente['idCliente']); ?>' class='btn btn-warning'>
+                                    <i class="fas fa-edit"></i> Editar
+                                </a>
+                                <a href='../controller/ClienteController.php?acao=excluir&id=<?= htmlspecialchars($cliente['idCliente']); ?>' class='btn btn-danger' onclick="return confirm('Tem certeza que deseja excluir? Esse cliente pode ter um serviço, e ele será excluido junto!')">
+                                    <i class="fas fa-trash"></i> Excluir
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control border border-info rounded" id="telefone" name="telefone" value="<?= htmlspecialchars($cliente['telefone']); ?>"required readonly>
-                    <label for="telefone">Telefone:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control border border-info rounded" id="cpf" name="cpf" value="<?= htmlspecialchars($cliente['cpf']); ?>" required readonly>
-                    <label for="cpf">CPF:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="date" class="form-control border border-info rounded" id="dataNascimento" name="dataNascimento" value="<?= htmlspecialchars($cliente['dataNascimento']); ?>" required readonly>
-                    <label for="dataNascimento">Data de Nascimento:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control border border-info rounded" id="endereco" name="endereco" value="<?= htmlspecialchars($cliente['endereco']); ?>" readonly>
-                    <label for="endereco">Endereço</label>
-                </div>
-            </div>
-
-             <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control border border-info rounded" id="bicicleta" name="bicicleta" value="<?= htmlspecialchars($cliente['bicicleta']); ?>" readonly>
-                    <label for="bicicleta">Bicicleta</label>
-                </div>
-            </div>
-
-            <div class="col-md-12 mb-3">
-                <div class="d-flex justify-content-end gap-2">
-                    <a href='formAtualizarCliente.php?id=<?= $idCliente ?>' class='btn btn-primary btn-sm'>Atualizar</a>
-                    <a href='../../controller/ClienteController.php?acao=excluir&id=<?= $idCliente?>' class='btn btn-danger btn-sm' onclick="return confirm('Tem certeza que deseja excluir? Esse cliente pode ter um serviço, e ele será excluido junto!')">Excluir</a>
-                    <a href="clientes.php" class="btn btn-secondary btn me-2">Voltar</a>
-                </div>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
-
-<?php include("../../app/footer.php"); ?>
+<?php include("../app/footer.php"); ?>
