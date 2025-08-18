f092635c471771459c28e7408bd4ef6af9236b6e
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-18 15:23:58 -0300

    Update formAtualizarCliente.php

--- view/ClienteView/formAtualizarCliente.php
+++ view/ClienteView/formAtualizarCliente.php
@@ -1,78 +1,73 @@
-<?php include(__DIR__."/../../app/header.php"); 
+<?php include(__DIR__."/../../app/header.php"); ?>
 
-include ('./../config/conexaoBD.php');
-include ('../../model/Cliente.php');
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white text-center">
+                    <h4 class="mb-0">Atualizar Cliente</h4>
+                </div>
+                <div class="card-body">
+                    <form action="../controller/ClienteController.php?acao=atualizar" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
+                        <input type="hidden" name="idCliente" value="<?= htmlspecialchars($cliente['idCliente']); ?>">
 
-$clienteModel = new Cliente($conn);
-if (isset($_GET['id'])) {
-    $idCliente = $_GET['id'];
-    
-    $cliente = $clienteModel->buscarClientePorId($idCliente);
-} else {
-    echo "ID do cliente não informado!";
-    exit(); 
-}
-?>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="nome" name="nome" value="<?= htmlspecialchars($cliente['nome']); ?>" placeholder="Nome completo" required>
+                                    <label for="nome">Nome:</label>
+                                    <div class="invalid-feedback">Por favor, preencha o nome.</div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="telefone" name="telefone" value="<?= htmlspecialchars($cliente['telefone']); ?>" placeholder="(xx) xxxxx-xxxx" required>
+                                    <label for="telefone">Telefone:</label>
+                                    <div class="invalid-feedback">Por favor, preencha o telefone.</div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cpf" name="cpf" value="<?= htmlspecialchars($cliente['cpf']); ?>" placeholder="xxx.xxx.xxx-xx">
+                                    <label for="cpf">CPF:</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="date" class="form-control" id="dataNascimento" name="dataNascimento" value="<?= htmlspecialchars($cliente['dataNascimento']); ?>">
+                                    <label for="dataNascimento">Data de Nascimento:</label>
+                                </div>
+                            </div>
 
-<div class="container-fluid">
-    <h4>Atualizar Cliente:</h4>
- 
-    <div class="col-sm-12">
-
-        <form action="../../controller/ClienteController.php?acao=atualizar" method="POST" enctype="multipart/form-data" class="was-validated">
-
-            <input type="hidden" name="idCliente" value="<?= $cliente['idCliente']; ?> ">
-
-            <div class="row">
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="nome" name="nome" value="<?= htmlspecialchars($cliente['nome']); ?>" required>
-                    <label for="nome">Nome:</label>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="endereco" name="endereco" value="<?= htmlspecialchars($cliente['endereco']); ?>" placeholder="Endereço">
+                                    <label for="endereco">Endereço</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="bicicleta" name="bicicleta" value="<?= htmlspecialchars($cliente['bicicleta']); ?>" placeholder="Marca e modelo da bicicleta">
+                                    <label for="bicicleta">Bicicleta</label>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <hr class="my-4">
+                        
+                        <div class="d-flex justify-content-between">
+                            <a href="javascript:history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </a>
+                            <button type="submit" class="btn btn-primary text-white">
+                                <i class="fas fa-edit"></i> Atualizar
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="telefone" name="telefone" value="<?= htmlspecialchars($cliente['telefone']); ?>"required>
-                    <label for="telefone">Telefone:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="cpf" name="cpf" value="<?= htmlspecialchars($cliente['cpf']); ?>" required>
-                    <label for="cpf">CPF:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="date" class="form-control" id="dataNascimento" name="dataNascimento" value="<?= htmlspecialchars($cliente['dataNascimento']); ?>" required>
-                    <label for="dataNascimento">Data de Nascimento:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="endereco" name="endereco" value="<?= htmlspecialchars($cliente['endereco']); ?>" >
-                    <label for="endereco">Endereço</label>
-                </div>
-            </div>
-
-             <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="bicicleta" name="bicicleta" value="<?= htmlspecialchars($cliente['bicicleta']); ?>">
-                    <label for="bicicleta">Bicicleta</label>
-                </div>
-            </div>
-
-            <div class="col-md-12 text-end">
-                <button type="submit" class="btn btn-primary">Atualizar</button>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
