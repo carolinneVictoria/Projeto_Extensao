f7f64d1b09010b79dabc989e29646519ab087912
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-18 15:23:51 -0300

    Update ClienteController.php

--- controller/ClienteController.php
+++ controller/ClienteController.php
@@ -5,7 +5,16 @@ include_once "../model/Cliente.php";
 $clienteModel = new Cliente($conn);
 
 function listarClientes($clienteModel) {
-    $clientes = $clienteModel->listarClientes();
+    $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+    $limite = 5; // clientes por página
+    $offset = ($paginaAtual - 1) * $limite;
+
+    // Busca clientes
+    $clientes = $clienteModel->listarClientesPaginados($limite, $offset);
+
+    // Conta total
+    $totalClientes = $clienteModel->contarClientes();
+    $totalPaginas = ceil($totalClientes / $limite);
     include('../view/ClienteView/clientes.php');
 }
 
@@ -19,7 +28,7 @@ function cadastrarCliente($clienteModel) {
         $bicicleta = $_POST['bicicleta'];
 
         if ($clienteModel->cadastrarCliente($nomeCliente, $telefoneCliente, $cpfCliente, $dataNascimento, $endereco, $bicicleta)){
-            header("Location: ../view/ClienteView/clientes.php");
+            listarClientes($clienteModel);
             exit();
         } else {
             echo "ERRO AO CADASTRAR CLIENTE.";
@@ -28,6 +37,7 @@ function cadastrarCliente($clienteModel) {
 }
 
 function atualizarCliente($clienteModel) {
+    
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['idCliente'])) {
         $idCliente = $_POST['idCliente'];
         $nome = $_POST['nome'];
@@ -38,19 +48,18 @@ function atualizarCliente($clienteModel) {
         $bicicleta = $_POST['bicicleta'];
 
         if ($clienteModel->atualizarCliente($idCliente, $nome, $telefone, $cpf, $dataNascimento, $endereco, $bicicleta)) {
-            header("Location: ../view/ClienteView/clientes.php");
+            listarClientes($clienteModel);
             exit();
         } else {
             echo "Erro ao atualizar o cliente!" . mysqli_error($clienteModel->getConnection());
         }
     }
 }
-
 function excluirCliente($clienteModel) {
     $idCliente = $_GET['id'];
     $resultado = $clienteModel->excluirCliente($idCliente);
     if($resultado) {
-        header('Location: ../view/ClienteView/clientes.php');
+        listarClientes($clienteModel);
         exit();
     } else {
         echo "Erro ao excluir. Possivelmente tem associações " ;
@@ -64,6 +73,40 @@ function buscarCliente($clienteModel) {
             include('../view/ClienteView/verBuscaCliente.php');
     } else {
         echo "NENHUM TERMO DE BUSCA INFORMADO.";
+    }
+}
+
+function verCliente($clienteModel){
+    if (isset($_GET['id'])) {
+        $idCliente = $_GET['id'];
+        $cliente = $clienteModel->buscarClientePorId($idCliente);
+
+        if ($cliente) {
+            include_once '../view/ClienteView/verCliente.php';
+        } else {
+            header('Location: ../view/ClienteView/clientes.php&erro=naoencontrado');
+            exit();
+        }
+    } else {
+        header('Location: ../view/ClienteView/clientes.php&erro=naoencontrado');
+        exit();
+    }
+}
+
+function formAtualizar($clienteModel){
+    if (isset($_GET['id'])) {
+        $idCliente = $_GET['id'];
+        $cliente = $clienteModel->buscarClientePorId($idCliente);
+
+        if ($cliente) {
+            include_once '../view/ClienteView/formAtualizarCliente.php';
+        } else {
+            header('Location: ../view/ClienteView/clientes.php&erro=naoencontrado');
+            exit();
+        }
+    } else {
+        header('Location: ../view/ClienteView/clientes.php&erro=naoencontrado');
+        exit();
     }
 }
 
@@ -81,6 +124,12 @@ if (isset($_GET['acao'])) {
         excluirCliente($clienteModel);
     } elseif($acao == 'buscar') {
         buscarCliente($clienteModel);
+    } elseif($acao == 'ver'){
+        verCliente($clienteModel);
+    } elseif ($acao == 'formAtualizar'){
+        formAtualizar($clienteModel);
+    } elseif ($acao == 'formCadastrar'){
+        include_once '../view/ClienteView/formCliente.php';
     }
 } else {
     // Caso nenhuma ação seja especificada, exibe a listagem
