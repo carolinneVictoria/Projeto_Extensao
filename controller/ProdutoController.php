11d814928424dc0ca35f5985004c73ea6786129b
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-24 18:35:47 -0300

    Update ProdutoController.php

--- controller/ProdutoController.php
+++ controller/ProdutoController.php
@@ -4,12 +4,10 @@ include_once "../config/conexaoBD.php";
 include_once '../model/Categoria.php';
 include_once '../model/CompraProduto.php';
 
-// Instanciando o Model
 $compraProdutoModel =new CompraProduto($conn);
 $produtoModel = new Produto($conn);
 $categoriaModel = new Categoria($conn);
 
-// Função para cadastrar o produto
 function cadastrarProduto($produtoModel) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nomeProduto = $_POST['nomeProduto'];
@@ -19,7 +17,7 @@ function cadastrarProduto($produtoModel) {
         $idCategoria = $_POST['idCategoria'];
 
         if ($produtoModel->cadastrarProduto($nomeProduto, $descricaoProduto, $quantidadeProduto, $valorProduto, $idCategoria)) {
-            header("Location: ../view/ProdutoView/produtos.php");
+            listarProdutos($produtoModel);
             exit();
         } else {
             echo "Erro ao cadastrar produto!";
@@ -42,21 +40,24 @@ function cadastrarProdutoCompra($produtoModel, $compraProdutoModel) {
         if ($idProduto) {
             $compraProdutoModel->adicionarProdutoCompra($idProduto,$idCompra,$quantidadeProduto,$valorProduto
             );
-            header("Location: ../view/CompraView/produtoCompra.php?id=" . $idCompra);
+            header("Location: /Projeto_Extensao/view/CompraView/produtoCompra.php?id=" . $idCompra);
             exit();
         } else {
             echo "Erro ao cadastrar produto!";
         }
     }
 }
-// Função para listar os produtos
-function listarProdutos($produtoModel, $categoriaModel) {
-    $produtos = $produtoModel->listarProdutos();
-    $categorias = $categoriaModel->listarCategoria();
+function listarProdutos($produtoModel) {
+    $paginaAtual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+    $limite = 8; // produtos por página
+    $offset = ($paginaAtual - 1) * $limite;
+
+    $produtos = $produtoModel->listarProdutosPaginados($limite, $offset);
+
+    $totalProdutos = $produtoModel->contarProdutos();
+    $totalPaginas = ceil($totalProdutos / $limite);
     include('../view/ProdutoView/produtos.php');
 }
-
-// Função para processar a atualização do produto
 function atualizarProduto($produtoModel) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['idProduto'])) {
         $idProduto = $_POST['idProduto'];
@@ -67,26 +68,23 @@ function atualizarProduto($produtoModel) {
         $idCategoria = $_POST['categoriaProduto']; //uso o mesmo name que o do formulario
 
         if ($produtoModel->atualizarProduto($idProduto, $nomeProduto, $descricaoProduto, $quantidadeProduto, $valorProduto, $idCategoria)) {
-            header("Location: ../view/ProdutoView/produtos.php");
+            listarProdutos($produtoModel);
             exit();
         } else {
             echo "Erro ao atualizar o produto!" . mysqli_error($produtoModel->getConnection());
         }
     }
 }
-
 function excluirProduto($produtoModel){
     $idProduto = $_GET['id'];
     $resultado = $produtoModel->excluirProduto($idProduto);
     if ($resultado) {
-    echo "Produto excluído com sucesso!";
-    header('Location: ../view/ProdutoView/produtos.php');
+    listarProdutos($produtoModel);
     exit();
     } else {
     echo "Erro ao excluir o Produto. Possivelmente tem associações.";
     }
 }
-
 function buscarProdutos($produtoModel, $categoriaModel) {
     if (isset($_GET['busca'])) {
         $termo = $_GET['busca'];
@@ -98,8 +96,47 @@ function buscarProdutos($produtoModel, $categoriaModel) {
         echo "Nenhum termo de busca informado.";
     }
 }
+function formCadastro($produtoModel, $categoriaModel){
+    $categorias = $categoriaModel->listarCategoria();
+    include_once '../view/ProdutoView/formProdutos.php';
+}
+function verProduto($produtoModel){
+    if (isset($_GET['id'])) {
+        $idProduto = $_GET['id'];
+        $produto = $produtoModel->buscarProdutoPorId($idProduto);
 
+        if ($produto) {
+            include_once '../view/ProdutoView/verProduto.php';
+        } else {
+            header('Location: ../view/ProdutoView/produtos.php&erro=naoencontrado');
+            exit();
+        }
+        } else {
+            header('Location: ../view/ProdutoView/produtos.php&erro=naoencontrado');
+            exit();
+        }
+}
+function formAtualizar($produtoModel, $categoriaModel){
+    if (isset($_GET['id'])) {
+        $idProduto = $_GET['id'];
+        $produto = $produtoModel->buscarProdutoPorId($idProduto);
+        $categoria = $categoriaModel->listarCategoria();
 
+        if ($produto) {
+            include_once '../view/ProdutoView/formAtualizarProdutos.php';
+        } else {
+            header('Location: ../view/ProdutoView/produtos.php&erro=naoencontrado');
+            exit();
+        }
+    } else {
+        header('Location: ../view/ProdutoView/produtos.php&erro=naoencontrado');
+        exit();
+    }
+}
+function formCompraProduto($produtoModel, $categoriaModel){
+    $categorias = $categoriaModel->listarCategoria();
+    include_once '../view/ProdutoView/formProdutosCompra.php';
+}
 // Determina qual ação chamar com base na URL ou método
 if (isset($_GET['acao'])) {
     $acao = $_GET['acao'];
@@ -108,7 +145,7 @@ if (isset($_GET['acao'])) {
     if ($acao == 'cadastrar') {
         cadastrarProduto($produtoModel);
     } elseif ($acao == 'listar') {
-        listarProdutos($produtoModel, $categoriaModel);
+        listarProdutos($produtoModel);
     } elseif ($acao == 'atualizar') {
         atualizarProduto($produtoModel); // Se o formulário for enviado, processa a atualização
     } elseif ($acao == 'excluir') {
@@ -117,9 +154,17 @@ if (isset($_GET['acao'])) {
         buscarProdutos($produtoModel, $categoriaModel);
     } elseif($acao == 'cadastrarCompra'){
         cadastrarProdutoCompra($produtoModel, $compraProdutoModel);
+    } elseif($acao == 'formCadastrar'){
+        formCadastro($produtoModel, $categoriaModel);
+    } elseif($acao == 'ver'){
+        verProduto($produtoModel);
+    } elseif($acao == 'formAtualizar'){
+        formAtualizar($produtoModel, $categoriaModel);
+    } elseif($acao == 'formCompraProduto'){
+        formCompraProduto($produtoModel, $categoriaModel);
     }
 } else {
     // Caso nenhuma ação seja especificada, exibe a listagem
-    listarProdutos($produtoModel, $categoriaModel);
+    listarProdutos($produtoModel);
 }
 ?>
