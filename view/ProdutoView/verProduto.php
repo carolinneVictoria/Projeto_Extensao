f40cf7a7f9d785a6295c8688d6a67dfff90f2c8c
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-24 18:36:16 -0300

    Update verProduto.php

--- view/ProdutoView/verProduto.php
+++ view/ProdutoView/verProduto.php
@@ -1,91 +1,73 @@
-<?php include("../../app/header.php");
+<?php include("../app/header.php"); ?>
 
-include ('../../config/conexaoBD.php');
-include ('../../model/Produto.php');
-include ('../../model/Categoria.php');
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-info text-white text-center">
+                    <h4 class="mb-0">Detalhes do Produto</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <input type="hidden" name="idProduto" value="<?= htmlspecialchars($produto['idProduto']); ?>">
 
-$produtoModel = new Produto($conn);
-$categoriaModel = new Categoria($conn);
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="nomeProduto" name="nomeProduto" value="<?= htmlspecialchars($produto['nomeProduto']); ?>" readonly placeholder="Nome do Produto">
+                                    <label for="nomeProduto">Nome do Produto:</label>
+                                </div>
+                            </div>
 
-if (isset($_GET['id'])) {
-    $idProduto = $_GET['id'];
-    
-    $produto = $produtoModel->buscarProdutoPorId($idProduto);
-    $categoria = $categoriaModel->listarCategoria();
-} else {
-    echo "ID do produto não informado!";
-    exit(); 
-}
-?>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="valorProduto" name="valorProduto" value="R$ <?= number_format($produto['valorProduto'], 2, ',', '.'); ?>" readonly placeholder="Valor do Produto">
+                                    <label for="valorProduto">Valor do Produto:</label>
+                                </div>
+                            </div>
 
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="categoria" name="categoria" value="<?= htmlspecialchars($produto['descricao']); ?>" readonly placeholder="Categoria">
+                                    <label for="categoria">Categoria:</label>
+                                </div>
+                            </div>
 
-<div class="container-fluid text-left">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="quantidadeProduto" name="quantidadeProduto" value="<?= htmlspecialchars($produto['quantidadeProduto']); ?>" readonly placeholder="Quantidade">
+                                    <label for="quantidadeProduto">Quantidade:</label>
+                                </div>
+                            </div>
+                            
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <textarea style="height: 100px" class="form-control" id="descricaoProduto" name="descricaoProduto" readonly placeholder="Descrição do Produto"><?= htmlspecialchars($produto['descricaoProduto']); ?></textarea>
+                                    <label for="descricaoProduto">Descrição do Produto:</label>
+                                </div>
+                            </div>
+                        </div>
 
-    <h3>Detalhes do Produto:</h3>
-    
-    <div class="col-sm-12">
-        
-        <input type="hidden" name="idProduto" value="<?= $produto['idProduto']; ?> ">
+                        <hr class="my-4">
 
-        <div class="row">
-        <div class="col-md-6 mb-3" >
-            <div class="form-floating">
-                <input type="text" class="form-control border border-info rounded" id="nomeProduto" name="nomeProduto" value="<?= $produto['nomeProduto']; ?>" required readonly>
-                <label for="nomeProduto">Nome do Produto:</label>
+                        <div class="d-flex justify-content-between">
+                            <button onclick="history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </button>
+                            <div>
+                                <a href='../controller/ProdutoController.php?acao=formAtualizar&id=<?= htmlspecialchars($produto['idProduto']); ?>' class='btn btn-warning me-2 text-white'>
+                                    <i class="fas fa-edit"></i> Editar
+                                </a>
+                                <a href='../controller/ProdutoController.php?acao=excluir&id=<?= htmlspecialchars($idProduto); ?>' class='btn btn-danger' onclick='return confirm("Tem certeza que deseja excluir? Produto tem associação com serviços, sendo assim ele também será excluído desse registro!")'>
+                                    <i class="fas fa-trash"></i> Excluir
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
-        <div class="col-md-6 mb-3">
-            <div class="form-floating">
-                <input type="text" class="form-control border border-info rounded" id="valorProduto" name="valorProduto" value="<?= $produto['valorProduto']; ?>" required readonly>
-                <label for="valorProduto">Valor do Produto:</label>
-            </div>
-        </div>
-
-        <div class="col-md-12 mb-3">
-            <div class="form-floating">
-                <textarea style="height: 100px" class="form-control border border-info rounded" id="descricaoProduto" name="descricaoProduto" readonly><?= $produto['descricaoProduto']; ?></textarea>
-                <label for="descricaoProduto">Descrição do Produto:</label>
-            </div>
-        </div>
-
-        <div class="col-md-6 mb-3">
-            <div class="form-floating">
-                <select class="form-control border border-info rounded" id="categoriaProduto" name="categoriaProduto" required readonly>
-                    <?php foreach ($categoria as $categorias): ?>
-                        <option value="<?= $categorias['idCategoria']; ?>" 
-                            <?= $categorias['idCategoria'] == $produto['idCategoria'] ? 'selected' : ''; ?>>
-                            <?= $categorias['descricao']; ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-            <label for="categoriaProduto">Categoria do Produto:</label>
-            </div>
-        </div>
-
-        <div class="col-md-6 mb-3">
-            <div class="form-floating">
-                <input type="text" class="form-control border border-info rounded" id="quantidadeProduto" name="quantidadeProduto" value="<?= $produto['quantidadeProduto']; ?>" required readonly>
-                <label for="quantidadeProduto">Quantidade:</label>
-            </div>
-        </div>
-
-        <div class="col-md-12 mb-3">
-            <div class="d-flex justify-content-end gap-2">
-                <a href='formAtualizarProdutos.php?id=<?= $idProduto ?>' class='btn btn-primary btn-sm'>Atualizar</a>
-                <a href='../../controller/ProdutoController.php?acao=excluir&id=<?= $idProduto ?>' class='btn btn-danger btn-sm' onclick='return confirm("Tem certeza que deseja excluir? Produto tem associação com serviços, sendo assim ele também sera excluido desse registro!")'>Excluir</a>
-                <a href="produtos.php" class="btn btn-secondary btn-sm">Voltar</a>
-            </div>
-        </div>
-
-        </div>
-
-        </div>
-
-    </form>
     </div>
-    
-
 </div>
 
-<?php include("../../app/footer.php") ?>
+<?php include("../app/footer.php") ?>
