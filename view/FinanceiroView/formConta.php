71e6d6d790beda2874e1dc745aa5a0a2d18386a6
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-25 17:20:04 -0300

    Update formConta.php

--- view/FinanceiroView/formConta.php
+++ view/FinanceiroView/formConta.php
@@ -1,51 +1,63 @@
-<?php include("../../app/header.php"); ?>
+<?php include("../app/header.php"); ?>
 
-<div class="container-fluid">
-    <h4>Cadastro de Conta:</h4>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white text-center">
+                    <h4 class="mb-0">Cadastro de Conta</h4>
+                </div>
+                <div class="card-body">
+                    <form action="/Projeto_Extensao/controller/FinanceiroController.php?acao=cadastrar"method="POST" enctype="multipart/form-data" class="was-validated">
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição" required>
+                                    <label for="descricao">Descrição:</label>
+                                </div>
+                            </div>
 
-    <div class="col-sm-12">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="valorTotal" name="valorTotal" placeholder="Valor" required>
+                                    <label for="valorTotal">Valor:</label>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="date" class="form-control" id="dataVencimento" name="dataVencimento" required>
+                                    <label for="dataVencimento">Data de Vencimento:</label>
+                                </div>
+                            </div>
 
-        <form action="/Projeto_Extensao/controller/FinanceiroController.php?acao=cadastrar" method="POST" enctype="multipart/form-data" class="was-validated">
-            <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <select class="form-select" id="status" name="status" required>
+                                        <option value="" selected disabled>-- Selecione o Status --</option>
+                                        <option value="1">Pago</option>
+                                        <option value="0">Pagamento não efetuado</option>
+                                    </select>
+                                    <label for="status">Status:</label>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <hr class="my-4">
 
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="descricao" name="descricao" required>
-                    <label for="descricao">Descrição:</label>
+                        <div class="d-flex justify-content-between">
+                            <a href="javascript:history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </a>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-save"></i> Cadastrar
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="valorTotal" name="valorTotal" required>
-                    <label for="valorTotal">Valor:</label>
-                </div>
-            </div>
-
-                <div class="col-md-6 mb-3">
-                    <div class="form-floating">
-                        <input type="date" class="form-control" id="dataVencimento" name="dataVencimento" required>
-                        <label for="dataVencimento">Data de Vencimento:</label>
-                    </div>
-                </div>
-
-                <div class="col-md-6 mb-3">
-                    <div class="form-floating">
-                        <select class="form-select" id="status" name="status" required>
-                            <option value="1">Pago!</option>
-                            <option value="0">Pagamento não efetuado!</option>
-                        </select>
-                        <label for="status">Entregue:</label>
-                    </div>
-                </div>
-
-            <div class="col-md-12 text-end">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
-<?php include("../../app/footer.php"); ?>
+<?php include("../app/footer.php"); ?>
