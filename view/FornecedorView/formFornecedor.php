b8c6a27960355d22e92c65359c4f1adcaf3bb10b
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-24 22:36:56 -0300

    Update formFornecedor.php

--- view/FornecedorView/formFornecedor.php
+++ view/FornecedorView/formFornecedor.php
@@ -1,51 +1,61 @@
-<?php include("../../app/header.php"); 
-session_start();
+<?php include("../app/header.php"); ?>
 
-?>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-primary text-white text-center">
+                    <h4 class="mb-0">Cadastro de Fornecedor</h4>
+                </div>
 
-<div class="container-fluid">
-    <h4>Cadastro de Fornecedor:</h4>
- 
-    <div class="col-sm-12">
+                <div class="card-body">
+                    <form action="/Projeto_Extensao/controller/FornecedorController.php?acao=cadastrar" method="POST" enctype="multipart/form-data" class="was-validated">
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cnpj" name="cnpj" placeholder="CNPJ" required>
+                                    <label for="cnpj">CNPJ:</label>
+                                </div>
+                            </div>
 
-        <form action="/Projeto_Extensao/controller/FornecedorController.php?acao=cadastrar" method="POST" enctype="multipart/form-data" class="was-validated">
-            <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" placeholder="Razão Social" required>
+                                    <label for="razaoSocial">Razão Social:</label>
+                                </div>
+                            </div>
 
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="cnpj" name="cnpj" required>
-                    <label for="cnpj">CNPJ:</label>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Endereço">
+                                    <label for="endereco">Endereço:</label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="telefone" name="telefone" placeholder="Telefone" required>
+                                    <label for="telefone">Telefone:</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr class="my-4">
+
+                        <div class="d-flex justify-content-between">
+                            <a href="javascript:history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </a>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-save"></i> Cadastrar
+                            </button>
+                        </div>
+
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" required>
-                    <label for="razaoSocial">Razão Social:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="endereco" name="endereco" >
-                    <label for="endereco">Endereço</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating">
-                    <input type="text" class="form-control" id="telefone" name="telefone" required>
-                    <label for="telefone">Telefone:</label>
-                </div>
-            </div>
-
-            <div class="col-md-12 text-end">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
-<?php include("../../app/footer.php"); ?>
+<?php include("../app/footer.php"); ?>
