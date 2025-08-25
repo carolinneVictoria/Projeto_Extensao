a6bbb497065cdbd54150d45d4111af1e679f6c56
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-24 22:37:06 -0300

    Update verFornecedor.php

--- view/FornecedorView/verFornecedor.php
+++ view/FornecedorView/verFornecedor.php
@@ -1,68 +1,66 @@
-<?php include("../../app/header.php"); 
-include ('./../config/conexaoBD.php');
-include ('../../model/Fornecedor.php');
+<?php include("../app/header.php"); ?>
 
-$fornecedorModel = new Fornecedor($conn);
-if (isset($_GET['id'])) {
-    $idFornecedor = $_GET['id'];
-    
-    $fornecedor = $fornecedorModel->buscarFornecedorPorId($idFornecedor);
-} else {
-    echo "ID do Fornecedor não informado!";
-    exit(); 
-}
-?>
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-info text-white text-center">
+                    <h4 class="mb-0">Detalhes do Fornecedor</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <input type="hidden" name="idFornecedor" value="<?= htmlspecialchars($fornecedor['idFornecedor']); ?>">
+                        
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="cnpj" name="cnpj" value="<?= htmlspecialchars($fornecedor['cnpj']); ?>" readonly placeholder="CNPJ">
+                                    <label for="cnpj">CNPJ:</label>
+                                </div>
+                            </div>
 
-<div class="container-fluid">
-    <h4>Detalhes do Fornecedor:</h4>
- 
-    <div class="col-sm-12">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" value="<?= htmlspecialchars($fornecedor['razaoSocial']); ?>" readonly placeholder="Razão Social">
+                                    <label for="razaoSocial">Razão Social:</label>
+                                </div>
+                            </div>
 
-        <form action="/Projeto_Extensao/controller/FornecedorController.php?acao=atualizar" method="POST" enctype="multipart/form-data" class="was-validated">
-        
-        <input type="hidden" name="idFornecedor" value="<?= $fornecedor['idFornecedor']; ?> ">
-        
-        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="endereco" name="endereco" value="<?= htmlspecialchars($fornecedor['endereco']); ?>" readonly placeholder="Endereço">
+                                    <label for="endereco">Endereço:</label>
+                                </div>
+                            </div>
 
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="cnpj" name="cnpj" value="<?= htmlspecialchars($fornecedor['cnpj']); ?>" required readonly>
-                    <label for="cnpj">CNPJ:</label>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="telefone" name="telefone" value="<?= htmlspecialchars($fornecedor['telefone']); ?>" readonly placeholder="Telefone">
+                                    <label for="telefone">Telefone:</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr class="my-4">
+
+                        <div class="d-flex justify-content-between">
+                            <button onclick="history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </button>
+                            <div>
+                                <a href='../controller/FornecedorController.php?acao=formAtualizar&id=<?= htmlspecialchars($idFornecedor); ?>' class='btn btn-warning me-2 text-white'>
+                                    <i class="fas fa-edit"></i> Editar
+                                </a>
+                                <a href='../controller/FornecedorController.php?acao=excluir&id=<?= htmlspecialchars($idFornecedor); ?>' class='btn btn-danger' onclick='return confirm("Tem certeza que deseja excluir?")'>
+                                    <i class="fas fa-trash"></i> Excluir
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="razaoSocial" name="razaoSocial" value="<?= htmlspecialchars($fornecedor['razaoSocial']); ?>" required readonly>
-                    <label for="razaoSocial">Razão Social:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="endereco" name="endereco" value="<?= htmlspecialchars($fornecedor['endereco']); ?>" readonly >
-                    <label for="endereco">Endereço</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="telefone" name="telefone" value="<?= htmlspecialchars($fornecedor['telefone']); ?>" required readonly>
-                    <label for="telefone">Telefone:</label>
-                </div>
-            </div>
-
-           <div class="col-md-12 mb-3">
-                <div class="d-flex justify-content-end gap-2">
-                    <a href='formAtualizarFornecedor.php?id=<?= $idFornecedor ?>' class='btn btn-primary btn-sm'>Atualizar</a>
-                    <a href='../../controller/FornecedorController.php?acao=excluir&id=<?= $idFornecedor ?>' class='btn btn-danger btn-sm' onclick='return confirm("Tem certeza que deseja excluir?")'>Excluir</a>
-                    <a href="fornecedores.php" class="btn btn-secondary btn me-2">Voltar</a>
-                </div>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
-<?php include("../../app/footer.php"); ?>
+<?php include("../app/footer.php"); ?>
