0febee55dee88468659ecc217abfb187967ae4b0
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-28 16:10:09 -0300

    Update verUsuario.php

--- view/UsuarioView/verUsuario.php
+++ view/UsuarioView/verUsuario.php
@@ -1,85 +1,63 @@
-<?php include(__DIR__ ."/../../app/header.php"); 
+<?php include("../app/header.php"); ?>
 
-include ('./../config/conexaoBD.php');
-include ('../../model/Usuario.php');
+<div class="container my-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card shadow-sm">
+                <div class="card-header bg-dark text-white text-center">
+                    <h4 class="mb-0">Detalhes do Usuário</h4>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <input type="hidden" name="idUsuario" value="<?= htmlspecialchars($usuario['idUsuario']); ?>">
+                        
+                        <div class="row g-3">
+                            <div class="col-12 text-center mb-3">
+                                <img src="/Projeto_Extensao/<?= htmlspecialchars($usuario['fotoUsuario']); ?>" alt="Foto do Usuário" class="img-fluid rounded-circle" style="width: 150px; height: 150px; object-fit: cover;">
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="nomeUsuario" name="nomeUsuario" value="<?= htmlspecialchars($usuario['nomeUsuario']); ?>" readonly placeholder="Nome do Usuário">
+                                    <label for="nomeUsuario">Nome:</label>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="telefoneUsuario" name="telefoneUsuario" value="<?= htmlspecialchars($usuario['telefoneUsuario']); ?>" readonly placeholder="Telefone">
+                                    <label for="telefoneUsuario">Telefone:</label>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-12">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="emailUsuario" name="emailUsuario" value="<?= htmlspecialchars($usuario['emailUsuario']); ?>" readonly placeholder="Email">
+                                    <label for="emailUsuario">Email:</label>
+                                </div>
+                            </div>
+                        </div>
 
-$usuarioModel = new Usuario($conn);
+                        <hr class="my-4">
 
-if (isset($_GET['id'])) {
-    $idUsuario = $_GET['id'];
-    
-    $usuario = $usuarioModel->buscarUsuarioPorId($idUsuario);
-} else {
-    echo "ID do usuário não informado!";
-    exit(); 
-}
-
-?>
-
-<div class="container-fluid">
-    <h3>Detalhes do Usuário:</h3>
-
-    <div class="col-sm-12">
-
-        <form action="/Projeto_Extensao/controller/UsuarioController.php?acao=atualizar" method="POST" enctype="multipart/form-data" class="was-validated">
-
-            <input type="hidden" name="idUsuario" value="<?= $usuario['idUsuario']; ?>">
-
-            <div class="row">
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="file" class="form-control" id="fotoUsuario" name="fotoUsuario" value="<?= htmlspecialchars($usuario['fotoUsuario']); ?>" readonly>
-                    <label for="fotoUsuario">Foto:</label>
+                        <div class="d-flex justify-content-between">
+                            <button onclick="history.back()" class="btn btn-secondary">
+                                <i class="fas fa-arrow-left"></i> Voltar
+                            </button>
+                            <div>
+                                <a href='../controller/UsuarioController.php?acao=formAtualizar&id=<?= $usuario['idUsuario']; ?>' class='btn btn-warning me-2 text-white'>
+                                    <i class="fas fa-edit"></i> Editar
+                                </a>
+                                <a href='../controller/UsuarioController.php?acao=excluir&id=<?= $usuario['idUsuario']; ?>' class='btn btn-danger' onclick='return confirm("Tem certeza que deseja excluir?")'>
+                                    <i class="fas fa-trash"></i> Excluir
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="nomeUsuario" placeholder="Informe o seu nome" name="nomeUsuario" value="<?= htmlspecialchars($usuario['nomeUsuario']); ?>" readonly>
-                    <label for="nomeUsuario">Nome:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="text" class="form-control" id="telefoneUsuario" placeholder="Informe o seu telefone" name="telefoneUsuario" value="<?= htmlspecialchars($usuario['telefoneUsuario']); ?>" readonly>
-                    <label for="telefoneUsuario">Telefone:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="email" class="form-control" id="emailUsuario" placeholder="Informe o seu email" name="emailUsuario" value="<?= htmlspecialchars($usuario['emailUsuario']); ?>" readonly>
-                    <label for="emailUsuario">Email:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="password" class="form-control" id="senhaUsuario" placeholder="Informe uma senha" name="senhaUsuario" value="<?= htmlspecialchars($usuario['senhaUsuario']); ?>" readonly>
-                    <label for="senhaUsuario">Senha:</label>
-                </div>
-            </div>
-
-            <div class="col-md-6 mb-3">
-                <div class="form-floating border border-info rounded">
-                    <input type="password" class="form-control" id="confirmarSenhaUsuario" placeholder="Confirme a senha" name="confirmarSenhaUsuario" value="<?= htmlspecialchars($usuario['senhaUsuario']); ?>" readonly>
-                    <label for="confirmarSenhaUsuario">Confirme a Senha:</label>
-                </div>
-            </div>
-
-            <div class="col-md-12 mb-3">
-                <div class="d-flex justify-content-end gap-2">
-                    <a href='formAtualizarUsuario.php?id=<?= $idUsuario ?>' class='btn btn-primary btn-sm'>Atualizar</a>
-                    <a href='../../controller/UsuarioController.php?acao=excluir&id=<?= $idUsuario ?>' class='btn btn-danger btn-sm' onclick='return confirm("Tem certeza que deseja excluir?")'>Excluir</a>
-                    <a href="usuarios.php" class="btn btn-secondary btn-sm">Voltar</a>
-                </div>
-            </div>
-
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
-<?php include(__DIR__ ."/../../app/footer.php"); ?>
+<?php include("../app/footer.php"); ?>
