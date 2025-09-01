ec39956cf43f0ca07680f8e92f95a5fdaa4d52a1
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-31 22:41:21 -0300

    Update Produto.php

--- model/Produto.php
+++ model/Produto.php
@@ -1,23 +1,18 @@
 <?php
-
 class Produto {
     private $conn;
 
     public function __construct($dbConnection) {
         $this->conn = $dbConnection;
     }
-
     public function getConnection() {
         return $this->conn;
     }
-
     public function listarProdutos() {
         $listarProdutos = "SELECT Produto.*, Categoria.descricao FROM Produto INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria ORDER BY Produto.nomeProduto ASC";
         $res = mysqli_query($this->conn, $listarProdutos);
         return $res;
     }
-
-    // Método para cadastrar um novo produto
     public function cadastrarProduto($nomeProduto, $descricaoProduto, $quantidadeProduto, $valorProduto, $idCategoria) {
     $sql = "INSERT INTO Produto (nomeProduto, descricaoProduto, quantidadeProduto, valorProduto, idCategoria)
             VALUES (?, ?, ?, ?, ?)";
@@ -38,10 +33,7 @@ class Produto {
 
     $stmt->close();
     return $this->conn->insert_id; // retorna o ID do novo produto inserido
-}
-
-
-    //Metódo para atualizar os detalhes de um produto./
+    }
     public function atualizarProduto($idProduto, $nomeProduto, $descricaoProduto, $quantidadeProduto, $valorProduto, $idCategoria){
         $atualizarProduto = "UPDATE Produto
                                 SET nomeProduto       = '$nomeProduto',
@@ -56,7 +48,6 @@ class Produto {
         $res = mysqli_query($this->conn, $atualizarProduto);
         return $res;
     }
-
     public function excluirProduto($idProduto){
         $query = "DELETE FROM Produto WHERE idProduto=?";
         $stmt = $this->conn->prepare($query);
@@ -67,40 +58,43 @@ class Produto {
     $stmt->bind_param("i", $idProduto);
     return ($stmt->execute());
     }
-
     public function buscarPorNome($termo) {
-    $buscarProduto = "SELECT Produto.*, Categoria.descricao
-                      FROM Produto
-                      INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria
-                      WHERE nomeProduto LIKE ? OR Categoria.descricao Like ?";
-    $stmt = $this->conn->prepare($buscarProduto);
-    $like = "%" . $termo . "%";
-    $stmt->bind_param("ss", $like, $like);
-    $stmt->execute();
-    $res = $stmt->get_result();
-    return $res;
-}
-
-// Método para buscar os detalhes de um produto específico
-    public function buscarProdutoporId($idProduto) {
-    $stmt = $this->conn->prepare("SELECT Produto.*, Categoria.descricao FROM Produto INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria WHERE Produto.idProduto = ?");
-    $stmt->bind_param("i", $idProduto);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($row = $result->fetch_assoc()) {
-        return $row; 
-    } else {
-        return null;
+        $buscarProduto = "SELECT Produto.*, Categoria.descricao
+                            FROM Produto
+                            INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria
+                            WHERE nomeProduto LIKE ? OR Categoria.descricao Like ?";
+        $stmt = $this->conn->prepare($buscarProduto);
+        $like = "%" . $termo . "%";
+        $stmt->bind_param("ss", $like, $like);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return $res;
     }
-}
-public function reduzirEstoque($idProduto, $quantidade) {
-    $sql = "UPDATE Produto SET quantidadeProduto = quantidadeProduto - ? WHERE id = ?";
-    $stmt = $this->conn->prepare($sql);
+    public function buscarProdutoporId($idProduto) {
+        $stmt = $this->conn->prepare("SELECT Produto.*, Categoria.descricao FROM Produto INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria WHERE Produto.idProduto = ?");
+        $stmt->bind_param("i", $idProduto);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()) {
+            return $row; 
+        } else {
+            return null;
+        }
+    }
+    public function reduzirEstoque($idProduto, $quantidade) {
+        $query = "UPDATE produto SET quantidadeProduto = quantidadeProduto - ? WHERE idProduto = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("ii", $quantidade, $idProduto);
+        return $stmt->execute();
+    }
+    public function aumentarEstoque($idProduto, $quantidade) {
+    $query = "UPDATE produto SET quantidadeProduto = quantidadeProduto + ? WHERE idProduto = ?";
+    $stmt = $this->conn->prepare($query);
     $stmt->bind_param("ii", $quantidade, $idProduto);
     return $stmt->execute();
-}
-public function listarProdutosPaginados($limite, $offset) {
+    }
+    public function listarProdutosPaginados($limite, $offset) {
         $sql = "SELECT Produto.*, Categoria.descricao
                     FROM Produto
                     INNER JOIN Categoria ON Produto.idCategoria = Categoria.idCategoria
@@ -117,6 +111,5 @@ public function listarProdutosPaginados($limite, $offset) {
         $row = $resultado->fetch_assoc();
         return $row['total'];
     }
-
 }
 ?>
