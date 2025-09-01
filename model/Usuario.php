f82a68d9d1761047581688ca2d8ddd46f73ff230
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-31 22:41:24 -0300

    Update Usuario.php

--- model/Usuario.php
+++ model/Usuario.php
@@ -5,11 +5,9 @@ class Usuario {
     public function __construct($dbConnection) {
         $this->conn = $dbConnection;
     }
-
     public function getConnection() {
         return $this->conn;
     }
-
     public function verificarLogin($emailUsuario, $senhaUsuario) {
         $query = "SELECT * FROM Usuario WHERE emailUsuario = ?";
         $stmt = $this->conn->prepare($query);
@@ -28,10 +26,7 @@ class Usuario {
             return false;
         }
     }
-
     public function cadastrarUsuario($fotoUsuario, $nomeUsuario, $telefoneUsuario, $emailUsuario, $senhaUsuario) {
-        
-
         $query = "INSERT INTO Usuario (fotoUsuario, nomeUsuario, telefoneUsuario, emailUsuario, senhaUsuario, tipoUsuario, statusUsuario)
                     VALUES ('$fotoUsuario', '$nomeUsuario', '$telefoneUsuario', '$emailUsuario', '$senhaUsuario', 'padrao', 'ativo')";
 
@@ -41,32 +36,29 @@ class Usuario {
         }
         return $res;
     }
-
     public function listarUsuarios() {
         $query = "SELECT * FROM Usuario";
         $res = mysqli_query($this->conn, $query);
         return $res;
     }
-
     public function buscarUsuarioPorId($idUsuario) {
-    $stmt = $this->conn->prepare("SELECT * FROM Usuario WHERE idUsuario = ?");
-    $stmt->bind_param("i", $idUsuario);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        $stmt = $this->conn->prepare("SELECT * FROM Usuario WHERE idUsuario = ?");
+        $stmt->bind_param("i", $idUsuario);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($row = $result->fetch_assoc()) {
-        return $row; // Retorna os dados do usuário
-    } else {
-        return null; // Caso o usuário não seja encontrado
+        if ($row = $result->fetch_assoc()) {
+            return $row;
+        } else {
+            return null;
+        }
     }
-}
     public function atualizarUsuario($idUsuario, $fotoUsuario, $nomeUsuario, $telefoneUsuario, $emailUsuario, $senhaUsuario) {
         $query = "UPDATE Usuario SET fotoUsuario=?, nomeUsuario=?, telefoneUsuario=?, emailUsuario=?, senhaUsuario=? WHERE idUsuario=?";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("sssssi", $fotoUsuario, $nomeUsuario, $telefoneUsuario, $emailUsuario, $senhaUsuario, $idUsuario);
         return $stmt->execute();
     }
-
     public function excluirUsuario($idUsuario) {
         $excluir = "DELETE FROM Usuario WHERE idUsuario=?";
         $stmt = $this->conn->prepare($excluir);
@@ -77,19 +69,17 @@ class Usuario {
         $stmt->bind_param("i", $idUsuario);
         return ($stmt->execute());
     }
-
     public function buscarPorNome($termo) {
-    $buscarUsuario = "SELECT *
-                        FROM Usuario
-                        WHERE nomeUsuario LIKE ?";
-    $stmt = $this->conn->prepare($buscarUsuario);
-    $like = "%" . $termo . "%";
-    $stmt->bind_param("s", $like);
-    $stmt->execute();
-    $res = $stmt->get_result();
-    return $res;
+        $buscarUsuario = "SELECT *
+                            FROM Usuario
+                            WHERE nomeUsuario LIKE ?";
+        $stmt = $this->conn->prepare($buscarUsuario);
+        $like = "%" . $termo . "%";
+        $stmt->bind_param("s", $like);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return $res;
     }
-
     public function listarUsuariosPaginados($limite, $offset) {
                 $sql = "SELECT * FROM Usuario LIMIT ? OFFSET ?";
                 $stmt = $this->conn->prepare($sql);
@@ -97,13 +87,11 @@ class Usuario {
                 $stmt->execute();
                 return $stmt->get_result();
     }
-
     public function contarUsuarios() {
             $sql = "SELECT COUNT(*) as total FROM Usuario";
             $resultado = $this->conn->query($sql);
             $row = $resultado->fetch_assoc();
             return $row['total'];
     }
-
 }
 ?>
