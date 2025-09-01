3748a88c23cff7c4dec48558361fef6c566dfe21
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-08-31 23:36:00 -0300

    Update Venda.php

--- model/Venda.php
+++ model/Venda.php
@@ -1,5 +1,4 @@
 <?php
-
 class Venda {
     private $conn;
 
@@ -10,35 +9,30 @@ class Venda {
     public function getConnection() {
         return $this->conn;
     }
-//Metodo para listar
-public function listarVendas() {
-    $listarVendas = "SELECT Venda.idVenda, Venda.data, Venda.valorTotal, Venda.formaPagamento, Usuario.nomeUsuario, Produto.nomeProduto
-                        FROM Venda
-                        INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
-                        INNER JOIN VendaProduto ON Venda.idVenda = VendaProduto.idVenda
-                        INNER JOIN Produto ON VendaProduto.idProduto = Produto.idProduto
-                        GROUP BY Venda.idVenda
-                        ORDER BY Venda.idVenda DESC; ";
-    $res = mysqli_query($this->conn, $listarVendas);
-    return $res;
-}
-
-    // Método para cadastrar
+    public function listarVendas() {
+        $listarVendas = "SELECT Venda.idVenda, Venda.data, Venda.valorTotal, Venda.formaPagamento, Usuario.nomeUsuario, Produto.nomeProduto
+                            FROM Venda
+                            INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
+                            INNER JOIN VendaProduto ON Venda.idVenda = VendaProduto.idVenda
+                            INNER JOIN Produto ON VendaProduto.idProduto = Produto.idProduto
+                            GROUP BY Venda.idVenda
+                            ORDER BY Venda.idVenda DESC; ";
+        $res = mysqli_query($this->conn, $listarVendas);
+        return $res;
+    }
     public function cadastrarVenda( $idUsuario, $data, $descontoVenda, $valorTotal, $formaPagamento) {
-    // Prepara a query com parâmetros
-    $stmt = $this->conn->prepare("INSERT INTO Venda (idUsuario, data, descontoVenda, valorTotal, formaPagamento)
-                                VALUES (?, ?, ?, ?, ?)");
-    if (!$stmt) {
-        die("Erro ao preparar: " . $this->conn->error);
+        $stmt = $this->conn->prepare("INSERT INTO Venda (idUsuario, data, descontoVenda, valorTotal, formaPagamento)
+                                    VALUES (?, ?, ?, ?, ?)");
+        if (!$stmt) {
+            die("Erro ao preparar: " . $this->conn->error);
+        }
+        $stmt->bind_param("issds", $idUsuario, $data, $descontoVenda, $valorTotal, $formaPagamento);
+        if ($stmt->execute()) {
+            return $this->conn->insert_id;
+        } else {
+            return false;
+        }
     }
-    $stmt->bind_param("issds", $idUsuario, $data, $descontoVenda, $valorTotal, $formaPagamento);
-    if ($stmt->execute()) {
-        return $this->conn->insert_id; // Retorna o ID da venda inserida
-    } else {
-        return false;
-    }
-}
-    //Metódo para atualizar
     public function atualizarVenda($idVenda, $idUsuario, $data, $descontoVenda, $valorTotal, $formaPagamento){
         $atualizarVenda = "UPDATE Venda
                                 SET idUsuario       = '$idUsuario',
@@ -53,7 +47,6 @@ public function listarVendas() {
         $res = mysqli_query($this->conn, $atualizarVenda);
         return $res;
     }
-
     public function excluirVenda($idVenda){
         $query = "DELETE FROM Venda WHERE idVenda=?";
         $stmt = $this->conn->prepare($query);
@@ -64,61 +57,55 @@ public function listarVendas() {
     $stmt->bind_param("i", $idVenda);
     return ($stmt->execute());
     }
-
     public function buscarPorNome($termo, $limite, $offset) {
-    $buscarPorNome = "SELECT Venda.*, Usuario.nomeUsuario, Produto.nomeProduto
-                        FROM Venda
-                        INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
-                        INNER JOIN VendaProduto ON Venda.idVenda = VendaProduto.idVenda
-                        INNER JOIN Produto ON VendaProduto.idProduto = Produto.idProduto
-                        WHERE Venda.valorTotal LIKE ?
-                            OR Produto.nomeProduto LIKE ?
-                            OR Usuario.nomeUsuario LIKE ?
-                        LIMIT ? OFFSET ?";
+        $buscarPorNome = "SELECT Venda.*, Usuario.nomeUsuario, Produto.nomeProduto
+                            FROM Venda
+                            INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
+                            INNER JOIN VendaProduto ON Venda.idVenda = VendaProduto.idVenda
+                            INNER JOIN Produto ON VendaProduto.idProduto = Produto.idProduto
+                            WHERE Venda.valorTotal LIKE ?
+                                OR Produto.nomeProduto LIKE ?
+                                OR Usuario.nomeUsuario LIKE ?
+                            LIMIT ? OFFSET ?";
 
-    $stmt = $this->conn->prepare($buscarPorNome);
+        $stmt = $this->conn->prepare($buscarPorNome);
 
-    if (!$stmt) {
-        die("Erro na query: " . $this->conn->error);
+        if (!$stmt) {
+            die("Erro na query: " . $this->conn->error);
+        }
+
+        $like = "%" . $termo . "%";
+        $stmt->bind_param("sssii", $like, $like, $like, $limite, $offset);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        return $res;
     }
-
-    $like = "%" . $termo . "%";
-    $stmt->bind_param("sssii", $like, $like, $like, $limite, $offset);
-    $stmt->execute();
-    $res = $stmt->get_result();
-    return $res;
-}
-
-
-// Método para buscar os detalhes
     public function buscarVendaPorId($idVenda) {
-    $stmt = $this->conn->prepare("SELECT Venda.*, Usuario.nomeUsuario
-                                    FROM Venda
-                                    INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
-                                    WHERE Venda.idVenda = ?");
-    $stmt->bind_param("i", $idVenda);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        $stmt = $this->conn->prepare("SELECT Venda.*, Usuario.nomeUsuario
+                                        FROM Venda
+                                        INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
+                                        WHERE Venda.idVenda = ?");
+        $stmt->bind_param("i", $idVenda);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($row = $result->fetch_assoc()) {
-        return $row;
-    } else {
-        return null;
+        if ($row = $result->fetch_assoc()) {
+            return $row;
+        } else {
+            return null;
+        }
     }
-}
     public function atualizarValorTotalVenda($idVenda, $valorTotal) {
         $query = "UPDATE Venda SET valorTotal = ? WHERE idVenda = ?";
         $stmt = $this->conn->prepare($query);
         $stmt->bind_param("di", $valorTotal, $idVenda);
         return $stmt->execute();
     }
-
     public function totalVendasPorMes($mes, $ano) {
         $sql = "SELECT SUM(valorTotal) AS totalVendas FROM venda WHERE MONTH(data) = ? AND YEAR(data) = ?";
 
         $stmt = $this->conn->prepare($sql);
         if (!$stmt) {
-            // Tratar erro aqui
             return 0;
         }
         $stmt->bind_param("ii", $mes, $ano);
@@ -128,7 +115,6 @@ public function listarVendas() {
 
         return $row['totalVendas'] ?? 0;
     }
-
     public function listarVendasPaginadas($limite, $offset) {
     $sql = "SELECT Venda.*, Usuario.nomeUsuario, Produto.nomeProduto FROM Venda
                         INNER JOIN Usuario ON Venda.idUsuario = Usuario.idUsuario
@@ -141,10 +127,7 @@ public function listarVendas() {
     $stmt->bind_param("ii", $offset, $limite);
     $stmt->execute();
     return $stmt->get_result();
-}
-
-
-    // Conta o total de registros
+    }
     public function contarVendas() {
         $sql = "SELECT COUNT(*) as total FROM Venda";
         $resultado = $this->conn->query($sql);
@@ -152,5 +135,4 @@ public function listarVendas() {
         return $row['total'];
     }
 }
-
 ?>
