044a333672aeb703f07ea59381efa6a32b7db9ac
Carolinne Victória <carolinevictoria1224@gmail.com>
2025-09-01 23:35:16 -0300

    Update CompraProduto.php

--- model/CompraProduto.php
+++ model/CompraProduto.php
@@ -1,40 +1,35 @@
 <?php
-
 class CompraProduto {
     private $conn;
 
     public function __construct($dbConnection) {
         $this->conn = $dbConnection;
     }
-
     public function getConnection() {
         return $this->conn;
     }
+    public function adicionarProdutoCompra($idProduto, $idCompra, $quantidade, $valorUnitario) {
+        $adicionarProduto = "INSERT INTO compraProduto (idProduto, idCompra, quantidade, valorUnitario)
+                                VALUES ('$idProduto', '$idCompra', '$quantidade', '$valorUnitario')";
 
-public function adicionarProdutoCompra($idProduto, $idCompra, $quantidade, $valorUnitario) {
-    $adicionarProduto = "INSERT INTO compraProduto (idProduto, idCompra, quantidade, valorUnitario)
-                            VALUES ('$idProduto', '$idCompra', '$quantidade', '$valorUnitario')";
-
-    $res = mysqli_query($this->conn, $adicionarProduto);
-    
-    if (!$res) {
-        echo "Erro SQL: " . mysqli_error($this->conn);
+        $res = mysqli_query($this->conn, $adicionarProduto);
+        
+        if (!$res) {
+            echo "Erro SQL: " . mysqli_error($this->conn);
+        }
+        return $res;
     }
-    return $res;
-}
-
-public function atualizarProdutoCompra($idCompra, $idProduto, $quantidade, $valorUnitario) {
-    $sql = "UPDATE compraProduto
-               SET quantidade    = ?,
-                   valorUnitario = ?
-             WHERE idCompra      = ?
-               AND idProduto     = ?";
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bind_param("diii", $quantidade, $valorUnitario, $idCompra, $idProduto);
-    return $stmt->execute();
-}
-
-public function excluirProdutoCompra($idCompra, $idProduto){
+    public function atualizarProdutoCompra($idCompra, $idProduto, $quantidade, $valorUnitario) {
+        $sql = "UPDATE compraProduto
+                SET quantidade    = ?,
+                    valorUnitario = ?
+                WHERE idCompra      = ?
+                AND idProduto     = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("diii", $quantidade, $valorUnitario, $idCompra, $idProduto);
+        return $stmt->execute();
+    }
+    public function excluirProdutoCompra($idCompra, $idProduto){
         $query = "DELETE FROM CompraProduto WHERE idCompra=? AND idProduto=?";
         $stmt = $this->conn->prepare($query);
         if ($stmt === false) {
@@ -44,9 +39,6 @@ public function excluirProdutoCompra($idCompra, $idProduto){
     $stmt->bind_param("ii", $idCompra, $idProduto);
     return ($stmt->execute());
     }
-
-
-
     public function listarProdutosCompra($idCompra) {
     $sql = "SELECT compraProduto.*, Produto.nomeProduto
             FROM compraProduto
@@ -57,18 +49,14 @@ public function excluirProdutoCompra($idCompra, $idProduto){
     $stmt->bind_param("i", $idCompra);
     $stmt->execute();
     return $stmt->get_result();
-}
-
-public function buscarProdutoCompra($idCompra, $idProduto) {
-    $sql = "SELECT compraProduto.quantidade, compraProduto.valorUnitario, Produto.nomeProduto
-            FROM compraProduto
-            JOIN Produto ON compraProduto.idProduto = Produto.idProduto
-            WHERE compraProduto.idCompra = ? AND compraProduto.idProduto = ?";
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bind_param("ii", $idCompra, $idProduto);
-    $stmt->execute();
-    return $stmt->get_result()->fetch_assoc();
-}
-
+    }
+    public function buscarProdutoCompra($idCompra, $idProduto) {
+            $query = "SELECT * FROM compraProduto WHERE idCompra = ? AND idProduto = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bind_param("ii", $idCompra, $idProduto);
+            $stmt->execute();
+            $resultado = $stmt->get_result();
+            return $resultado->fetch_assoc();
+    }
 }
 ?>
